dc05dc4ff0c5a4a417e8bf7089da36753aebacbf
Parthiv Kumar Das <130219761+parthiv002@users.noreply.github.com>
2024-03-28 18:52:07 +0530

    fixed ui issues

--- app/src/main/java/com/example/restaurant_page/MainActivity.kt
+++ app/src/main/java/com/example/restaurant_page/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.restaurant_page
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
     }
 }
