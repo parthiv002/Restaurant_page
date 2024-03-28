b0c591ad9b4a88a2fd76a4a524ca6bc5d6741599
Parthiv Kumar Das <130219761+parthiv002@users.noreply.github.com>
2024-03-28 20:08:48 +0530

    ui imrpoved

--- app/src/main/java/com/example/restaurant_page/MainActivity.kt
+++ app/src/main/java/com/example/restaurant_page/MainActivity.kt
@@ -3,15 +3,20 @@ package com.example.restaurant_page
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+//        val toolbar = findViewById<Toolbar>(R.id.custom_toolbar)
+//        setSupportActionBar(toolbar)
+
         window.setFlags(
-            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN,
+            WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
         )
     }
 }
